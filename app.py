90ecf82007517f28bd5da92f0e8c5bda4109c3dc
RomanLatipov <romanlatipov@gmail.com>
2024-08-22 23:23:42 -0400

    turn base64 to image and run through model

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import os
+from tensorflow.keras.models import load_model
+from matplotlib import pyplot as plt
+import os, cv2, tensorflow as tf, numpy as np
+import base64
+
 
 app = Flask(__name__)
 CORS(app)
 
+def base64ToImage(string64):
+    image_base64 = base64.b64decode(string64)
+    im_arr = np.frombuffer(image_base64, dtype=np.uint8)
+    img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+    resize = tf.image.resize(img, (256, 256))
+    resize = tf.image.resize(img, (256, 256))
+    new_model = load_model(os.path.join('models', 'happysadmodel.h5'))
+    yhat = new_model.predict(np.expand_dims(resize/255, 0))
+    if yhat > 0.5:
+        print('Predicted class is happy')
+    else:
+        print('Predicted class is sad')
+    print(yhat)
+
+
+
 @app.post("/upload")
 def test():
     test = request.json["string"]
-    print(test)
+    base64ToImage(test)
     return jsonify(test)
    
 
--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let fileInput = document.querySelector(".default-file-input");
 let string64 = "";
 fileInput.addEventListener("change", e => {
     const img = fileInput.files[0];
-    console.log(img)
+    // console.log(img)
     const reader = new FileReader();
     reader.readAsDataURL(img);
-    console.log(reader)
+    // console.log(reader)
     reader.onload = function(e) {
         string64 = reader.result.replace("data:", "").replace(/^.+,/, "");
-        console.log(string64);
+        // console.log(string64);
     }
 });
 
@@ -25,6 +25,5 @@ function uploadImage(event) {
         })
     })
     .then(res => res.json())
-    .then(data => console.log("successful return: "+ data ))
-    
+    .then(data => console.log("successful return: ")) 
 }
