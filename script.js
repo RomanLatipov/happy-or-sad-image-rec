d4b21d1db7d9fce57a789feaabcfb11737ecaa1e
RomanLatipov <romanlatipov@gmail.com>
2024-08-26 23:46:31 -0400

    add drag and drop functionality

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let fileInput = document.querySelector(".default-file-input");
-let string64 = "";
-fileInput.addEventListener("change", e => {
-    const img = fileInput.files[0];
-    // console.log(img)
+const fileInput = document.querySelector(".default-file-input");
+const dragDropText = document.querySelector(".dynamic-message");
+const draggableFileArea = document.querySelector(".drag-file-area");
+
+function toBase64(img) {
     const reader = new FileReader();
     reader.readAsDataURL(img);
     // console.log(reader)
@@ -10,6 +10,26 @@ fileInput.addEventListener("change", e => {
         string64 = reader.result.replace("data:", "").replace(/^.+,/, "");
         // console.log(string64);
     }
+}
+
+draggableFileArea.addEventListener("dragover", function(event) {
+    event.preventDefault();
+})
+
+draggableFileArea.addEventListener("drop", event => {
+    event.preventDefault();
+    console.log(event);
+    const imgFile = event.dataTransfer.files[0];
+    toBase64(imgFile);
+    dragDropText.innerHTML = 'File Dropped Successfully!';
+})
+
+let string64 = "";
+
+fileInput.addEventListener("change", e => {
+    const imgFile = fileInput.files[0];
+    toBase64(imgFile);
+    
 });
 function dislpayEmote(emote) {
     const emoteText = document.querySelector(".emote");
