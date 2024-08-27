1ccd61e14d4c37d7ce8e81cf9a7c18374bd0369e
RomanLatipov <romanlatipov@gmail.com>
2024-08-26 22:24:57 -0400

    moved base64 to image function to test.py

--- app.py
+++ app.py
@@ -1,44 +1,21 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from tensorflow.keras.models import load_model
-from matplotlib import pyplot as plt
-import os, cv2, tensorflow as tf, numpy as np
-import base64
-import asyncio
-
+# from tensorflow.keras.models import load_model
+# from matplotlib import pyplot as plt
+# import os, cv2, tensorflow as tf, numpy as np
+# import base64
+# # import asyncio
+from test import base64ToImage
 
 app = Flask(__name__)
 CORS(app)
-
-async def base64ToImage(string64):
-    image_base64 = base64.b64decode(string64)
-    im_arr = np.frombuffer(image_base64, dtype=np.uint8)
-    img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
-
-    # plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    # plt.show()
-
-    resize = tf.image.resize(img, (256, 256))
-    resize = tf.image.resize(img, (256, 256))
-    new_model = load_model(os.path.join('models', 'happysadmodel.h5'))
-    yhat = new_model.predict(np.expand_dims(resize/255, 0))
-    if yhat > 0.5:
-        print('Predicted class is happy')
-        return {"response": "happy"}
-    else:
-        print('Predicted class is sad')
-        return {"response": "sad"}
-
-
 
 @app.post("/upload")
 async def test():
     test = request.json["string"]
     response = await base64ToImage(test)
-    print (response)
+    # print (response)
     return jsonify(response)
-   
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 from tensorflow.keras.models import load_model
 from matplotlib import pyplot as plt
-import os, cv2, tensorflow as tf, numpy as np
+import os, cv2, tensorflow as tf, numpy as np, base64
 
-img = cv2.imread('sadtest.jpg')
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-# plt.show()
+async def base64ToImage(string64):
+    image_base64 = base64.b64decode(string64)
+    im_arr = np.frombuffer(image_base64, dtype=np.uint8)
+    img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
 
-resize = tf.image.resize(img, (256, 256))
-# plt.imshow(resize.numpy().astype(int))
-
-new_model = load_model(os.path.join('models', 'happysadmodel.h5'))
-yhat = new_model.predict(np.expand_dims(resize/255, 0))
-
-if yhat > 0.5:
-    print('Predicted class is happy')
-else:
-    print('Predicted class is sad')
+    # plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    # plt.show()
+    
+    resize = tf.image.resize(img, (256, 256))
+    new_model = load_model(os.path.join('models', 'happysadmodel.h5'))
+    yhat = new_model.predict(np.expand_dims(resize/255, 0))
+    if yhat > 0.5:
+        # print('Predicted class is happy')
+        return {"response": "happy"}
+    else:
+        # print('Predicted class is sad')
+        return {"response": "sad"}
